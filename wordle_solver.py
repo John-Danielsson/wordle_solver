569524527698e1f353dcef36a3eaad065c7d9ca7
John Danielsson <johnaugustd@gmail.com>
2023-10-16 00:10:16 -0700

    update wordle solver

--- wordle_solver.py
+++ wordle_solver.py
@@ -1,49 +1,43 @@
-ATTEMPTS = 6
-
-explainer = """
-This is the Wordle helper. It will help you solve the NYT wordle game.
-Here is how to use it:
-- Type in what you typed in the Wordle game.
-    - E.g. if you wrote "adieu", write "adieu".
-- If Wordle indicates that there is no A or I in the word,
-but the D and U are in the secret word (just in the wrong place)
-and the E is in the right place, type this in:
-01021
-    - 0 = gray
-    - 1 = yellow
-    - 2 = green
-The program will find all the words that meet this criteria.
-"""
+from re import match
 
 
-def trim_dictionary(dictionary: set, guess: str, condition: str) -> set:
-    pass
-    # for word in dictionary:
-    #     hits = 0
-    #     for i in range(5):
-    #         if condition[i] > 0
+class WordleHelper:
+    def __init__(self):
+        with open("dictionary.txt", 'r') as file:
+            content = file.read()
+        self.dict = content.split("\n")
+        self.guesses = []
 
-def play(dictionary: set) -> None:
-    for i in range(ATTEMPTS):
-        input1 = ""
-        while len(input1) != 5:
-            input1 = input("Type in your guess:")
-        input2 = ""
-        while len(input2) != 5:
-            input2 = input("Type in the colors:")
-        print(
-            trim_dictionary(
-                dictionary=dictionary,
-                guess=input1,
-                condition=input2
-            )
-        )
-    print("Game over.")
+    def pattern_match(self, hint, word, excluded):
+        if match(hint.replace("-", "[a-z]"), word) is not None:
+            return all(c not in excluded for c in word)
+        return False
+
+    def add_guess(self, word, hint):
+        self.guesses.append([word, hint])
+
+    def matches(self):
+        excluded = set()
+        green_letters = ["-", "-", "-", "-", "-"]
+        for i in range(len(self.guesses)):
+            for j in range(5):
+                if self.guesses[i][1][j] == "0":
+                    excluded.add(self.guesses[i][0][j])
+                elif self.guesses[i][1][j] == "2":
+                    green_letters[j] = self.guesses[i][0][j]
+        pattern = "".join(green_letters)
+        result = []
+        for w in self.dict:
+            if self.pattern_match(pattern, w, excluded):
+                result.append(w)
+        return result
 
 
 if __name__ == "__main__":
-   print(explainer)
-   words = open(file="dictionary.txt", mode="r")
-   dictionary = list(word[:5] for word in words)
-   # print(dictionary)
-   play(dictionary)
+    wh = WordleHelper()
+    wh.add_guess("adieu", "10010")
+    wh.add_guess("heart", "02200")
+    wh.add_guess("beans", "02200")
+    wh.add_guess("leafy", "22202")
+    print(len(wh.dict))
+    print(wh.matches())
